66d0d97187c4b1aa7fa08870b26f6f3e6244114e
virgie27 <virgie@hbict.io>
2023-11-16 07:39:33 +0800

    Use products.js

--- scripts/amzon.js
+++ scripts/amzon.js
@@ -1,36 +1,6 @@
+let productsHTML = "";
 
-
-
-const product = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}]
-
-let productsHTML = '';
-
-product.forEach((product) => {
+products.forEach((product) => {
   productsHTML += `<div class="product-container">
   <div class="product-image-container">
     <img class="product-image"
@@ -43,7 +13,7 @@ product.forEach((product) => {
 
   <div class="product-rating-container">
     <img class="product-rating-stars"
-      src="images/ratings/rating-${product.rating.stars *10}.png">
+      src="images/ratings/rating-${product.rating.stars * 10}.png">
     <div class="product-rating-count link-primary">
       ${product.rating.count}
     </div>
@@ -79,9 +49,8 @@ product.forEach((product) => {
     Add to Cart
   </button>
 </div>`;
-
 });
 
 console.log(productsHTML);
 
-document.querySelector('.js-product-grid').innerHTML = productsHTML;
+document.querySelector(".js-product-grid").innerHTML = productsHTML;
